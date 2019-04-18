db849d546ad2e9b837420be9ad6e50b86629f474
yanfangzero <zeroorez55@berkeley.edu>
2019-04-18 17:05:43 -0400

    batcher change

--- src/batcher.py
+++ src/batcher.py
@@ -36,10 +36,22 @@ import data
 FLAGS = tf.app.flags.FLAGS
 
 
-BERT_MODEL = 'uncased_L-12_H-768_A-12'
-BERT_MODEL_HUB = 'https://tfhub.dev/google/bert_' + BERT_MODEL + '/1'
+BERT_MODEL_HUB = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
 
-tokenizer = run_classifier_with_tfhub.create_tokenizer_from_hub_module(BERT_MODEL_HUB)
+def create_tokenizer_from_hub_module():
+  """Get the vocab file and casing info from the Hub module."""
+  with tf.Graph().as_default():
+    bert_module = hub.Module(BERT_MODEL_HUB)
+    tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+    with tf.Session() as sess:
+      vocab_file, do_lower_case = sess.run([tokenization_info["vocab_file"],
+                                            tokenization_info["do_lower_case"]])
+      
+  return bert.tokenization.FullTokenizer(
+      vocab_file=vocab_file, do_lower_case=do_lower_case)
+
+tokenizer = create_tokenizer_from_hub_module()
+#tokenizer = run_classifier_with_tfhub.create_tokenizer_from_hub_module(BERT_MODEL_HUB)
 
 
 class Example(object):
